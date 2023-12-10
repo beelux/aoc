0fc4aaa883cd9e4a8f77859741a95e6285eb9483
beelux <75177432+luxemboye@users.noreply.github.com>
2023-12-10 05:13:14 +0100

    2023 day08 part2

--- 2023/day08/day08.py
+++ 2023/day08/day08.py
@@ -34,8 +34,46 @@ def part1(lines) -> int:
 
     return steps
 
-def part2(lines):
-    return "TODO"
+def fetch_starting_nodes(nodes: dict[str, tuple[str, str]]) -> list[str]:
+    starting_nodes = []
+    for node in nodes:
+        if(node.endswith("A")):
+            starting_nodes.append(node)
+    return starting_nodes
 
-print("Part 1: \"" + str(part1(file_input())) + "\"")
-print("Part 2: \"" + part2(file_input()) + "\"")
+def ends_with_z(current_nodes: list[str]) -> bool:
+    for node in current_nodes:
+        if(not node.endswith("Z")):
+            return False
+    return True
+
+import math
+from six.moves import reduce
+
+def part2(lines):
+    steps = 0
+    tmp = parse_nodes(lines)
+    commands = tmp[0]
+    nodes = tmp[1]
+
+    current_nodes = fetch_starting_nodes(nodes)
+    least_steps : list[int] = [-1] * len(current_nodes)
+    while(-1 in least_steps):
+        current_command = commands[steps % len(commands)]
+        steps += 1
+        for index in range(len(current_nodes)):
+            match current_command:
+                case 'L':
+                    current_nodes[index] = nodes[current_nodes[index]][0]
+                case 'R':
+                    current_nodes[index] = nodes[current_nodes[index]][1]
+            if(least_steps[index] == -1 and current_nodes[index].endswith("Z")):
+                least_steps[index] = steps
+    multiplication = reduce(lambda x, y: x*y, least_steps)
+    gcd = math.gcd(*least_steps)
+
+    return math.lcm(*least_steps)
+
+#print("Part 1: \"" + str(part1(file_input())) + "\"")
+# First try: 155388722128598496968704 - too high
+print("Part 2: \"" + str(part2(file_input())) + "\"")
